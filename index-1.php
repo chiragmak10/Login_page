018184002bd83de9edfb71515afcadfb2b11b77d
Chirag Makdiya <chiragmak100@gmail.com>
2019-09-04 21:03:42 +0530

    Update index-1.php

--- index-1.php
+++ index-1.php
@@ -1,9 +1,9 @@
 
 <?php
 
-$use=$_POST['use'];
-$pass=$_POST['pass'];
-$con=mysqli_connect("localhost","root","","reg") or die(mysqli_error());
+$use=$_POST['use']; // Name assinged to input field in HTML page
+$pass=$_POST['pass'];// Name assinged to input field in HTML page
+$con=mysqli_connect("localhost","root","","reg") or die(mysqli_error()); // Creation of Connection 
 $query = "select * from reg where username='".$use."' and password='".$pass."';";
 $result=mysqli_query($con,$query);
 $resultCheck=mysqli_num_rows($result);
@@ -11,7 +11,6 @@ if($resultCheck>0)
 {
 	echo "<script type='text/javascript'>alert('login successfully');</script>";
 		#echo "login successfully";
-
 
 }
 else{
